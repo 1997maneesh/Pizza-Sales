27037e57372d0963e49a7929cd9ca3fde77126f3
Maneesh kumar <mohitchouhan474@outlook.com>
2025-08-16 14:21:55 +0530

    Key & insights.sql

--- Key & insights.sql
+++ Key & insights.sql
@@ -95,3 +95,59 @@ GROUP BY Pizza_name
 ORDER BY Total_quantity_sold DESC
 LIMIT 3;
 
+-- The highest value pizza order is Order ID #15674, with a total order value of $83 and quantity sold 4 to the The California Chicken Pizza
+
+SELECT 
+    od.order_id,
+    pt.name AS pizza_name, od.quantity as quantity,
+    SUM(od.quantity * p.price) AS order_value
+FROM order_details od
+JOIN pizzas p 
+    ON od.pizza_id = p.pizza_id
+JOIN pizza_types pt 
+    ON pt.pizza_type_id = p.pizza_type_id
+GROUP BY od.order_id, pt.name, quantity
+ORDER BY order_value DESC
+LIMIT 1;
+
+-- On average, each order contains around 2.32 pizzas
+
+SELECT 
+    ROUND(SUM(quantity) / COUNT(DISTINCT order_id),
+            2) AS avg_pizza_per_order
+FROM
+    order_details;
+
+-- Total Quantity by Category
+-- Classic	14888
+-- Supreme	11987
+-- Veggie	11649
+-- Chicken	11050
+
+SELECT 
+    pt.category AS category, SUM(od.quantity) AS total_quantity
+FROM
+    pizza_types AS pt
+        JOIN
+    pizzas AS p ON pt.pizza_type_id = p.pizza_type_id
+        JOIN
+    order_details AS od ON od.pizza_id = p.pizza_id
+GROUP BY category
+ORDER BY total_quantity DESC;
+
+-- The highest number of pizzas were ordered 12 to 13 PM
+-- 12	11832149
+-- 13	11694198
+-- 18	11338388
+-- 17	10683107
+-- 19	9505830
+-- 16	9029757
+
+SELECT 
+    HOUR(o.time) AS hours, SUM(od.order_details_id) AS orders
+FROM
+    orders AS o
+        JOIN
+    order_details AS od ON o.order_id = od.order_details_id
+GROUP BY hours
+ORDER BY orders DESC;
